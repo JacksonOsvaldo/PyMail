ae8b32e188a276ef26741d3401158fde14945cbb
Jackson Osvaldo <jacksonosvaldo@live.com>
2019-01-15 17:33:09 -0300

    Initial Commit

--- main.py
+++ main.py
@@ -8,9 +8,6 @@
 
 from PyQt5 import QtCore, QtGui, QtWidgets
 
-import send as sd
-
-
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
